2fac2b877b6ec86f7d572e48bd2c258e431803a8
Luis <lmeridad2000@alumno.ipn.mx>
2026-05-24 20:44:52 -0600

    Redirect root to demo company

--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,7 @@
-import Sidebar from '@/components/layout/Sidebar'
-import Topbar from '@/components/layout/Topbar'
-import TrialBanner from '@/components/layout/TrialBanner'
-import PageHead from '@/components/layout/PageHead'
-import KpiCards from '@/components/dashboard/KpiCards'
-import ProtocolCompliance from '@/components/dashboard/ProtocolCompliance'
-import AiInsights from '@/components/dashboard/AiInsights'
-import ScoreTrend from '@/components/dashboard/ScoreTrend'
-import ChannelBreakdown from '@/components/dashboard/ChannelBreakdown'
-import InteractionsTable from '@/components/dashboard/InteractionsTable'
-import CtaStrip from '@/components/dashboard/CtaStrip'
-import { protocolData, insightsData, channelMix, interactions } from '@/lib/data'
+import { redirect } from 'next/navigation'
 
-const demoKpis = {
-  qualityScore:      6.2,
-  responseRatePct:   64,
-  responseMins:      8,
-  responseSecs:      32,
-  closePct:          18,
-  totalInteractions: 217,
-}
+const DEMO_COMPANY_ID = 'a37ae6ad-3967-4835-82aa-dd8d7072da22'
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen" style={{ gridTemplateColumns: '232px 1fr' }}>
-      <Sidebar />
-      <main className="flex flex-col min-w-0">
-        <Topbar />
-        <TrialBanner />
-        <section className="px-8 pt-6 pb-12 flex flex-col gap-[22px]">
-          <PageHead />
-          <KpiCards data={demoKpis} />
-
-          <div className="grid gap-4" style={{ gridTemplateColumns: '1.4fr 1fr' }}>
-            <ProtocolCompliance data={protocolData} />
-            <AiInsights data={insightsData} />
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
-            <ScoreTrend />
-            <ChannelBreakdown data={channelMix} />
-          </div>
-
-          <InteractionsTable data={interactions} />
-          <CtaStrip />
-        </section>
-      </main>
-    </div>
-  )
+  redirect(`/${DEMO_COMPANY_ID}`)
 }
